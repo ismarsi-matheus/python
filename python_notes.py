c600bc59e239c0b1997228df78e08caf8f818ec8
ismarsi-matheus <matheusleo.senac@gmail.com>
2024-10-27 15:30:48 -0300

    Add files via upload

---
+++ python_notes.py
@@ -0,0 +1,39 @@
+#entrada de dados /ingredientes #
+#processamento de dados/receita#
+#saida de dados/bolinho#
+
+#saida de dados#
+print('hello world')
+#entrada de dados#
+input("qual é a liguagem de programacao que voce esta aprendendo")
+
+"""
+Isso é um comrntário
+isso aqui ainda é um comentrio
+"""
+
+tipos de variaveis
+1-int-numeros inteiros,sem parte decimal 
+2-float numeros reais ,ou seja, com a parte decimal 
+3-str cadeia de caracteres ,dados textuais
+4-bool valores logicos(booleanos),true or false ,verdadeiro ou falso 
+
+idade =26
+altura =1.77
+nome ='matheus '
+estudando=true
+print(type(idade))
+print(type(altura))
+print(type(nome))
+print(type(estudando))
+
+#obtendo dados do usuario e salvando nas variaveis #
+
+linguagem = input('quak é a linguagem de progrmaçao que voce esta estudando')
+print(' a linguagem que voce esta estudando é ',linguagem)
+
+
+
+
+
+
